2bc95746238b00ec4ef321e8b44d19cbf94addb9
Polawat Huang <polawat_h@seasiacenter.com>
2024-03-19 10:43:25 +0700

    Close basePath to next.config

--- next.config.mjs
+++ next.config.mjs
@@ -7,8 +7,8 @@ import remarkGfm from 'remark-gfm'
 let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 
 const nextConfig = {
-  basePath: '/portfolio',
-  assetPrefix: '/portfolio/',
+  // basePath: '/portfolio',
+  // assetPrefix: '/portfolio/',
   output:  process.env.NODE_ENV !== "production" ? undefined: "export",
   images: {
     unoptimized: envImageUnoptimize,
