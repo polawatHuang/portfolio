3985d753741d92d427c778a5ffba8c501488c243
Polawat Huang <polawat_h@seasiacenter.com>
2024-03-19 10:13:45 +0700

    Config next.config and add FadeIn animation

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,16 @@ import nextMDX from '@next/mdx'
 import remarkGfm from 'remark-gfm'
 
 /** @type {import('next').NextConfig} */
+
+let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
+
 const nextConfig = {
   basePath: '/portfolio',
   assetPrefix: '/portfolio/',
+  output:  process.env.NODE_ENV !== "production" ? undefined: "export",
+  images: {
+    unoptimized: envImageUnoptimize,
+  },
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'mdx'],
 }
 
