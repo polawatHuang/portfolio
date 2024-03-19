d565c8759754d9bfd1f3ed727dc7f37be72a5b10
Polawat Huang <polawat_h@seasiacenter.com>
2024-03-19 15:02:01 +0700

    Updates

---
+++ _next/static/chunks/app/speaking/page-709680f8804f8dbd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[701,160,453],{7442:function(n,e,u){Promise.resolve().then(u.t.bind(u,233,23))}},function(n){n.O(0,[233,306,89,744],function(){return n(n.s=7442)}),_N_E=n.O()}]);
