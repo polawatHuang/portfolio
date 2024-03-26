dc8d1a36ba7480045f00162c9a6684d28f31f514
Polawat Huang <polawat_h@seasiacenter.com>
2024-03-26 13:31:22 +0700

    close article.ts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -1,4 +1,4 @@
-import glob from 'fast-glob'
+// import glob from 'fast-glob'
 
 interface Article {
   title: string
@@ -11,26 +11,26 @@ export interface ArticleWithSlug extends Article {
   slug: string
 }
 
-async function importArticle(
-  articleFilename: string,
-): Promise<ArticleWithSlug> {
-  let { article } = (await import(`../app/articles/${articleFilename}`)) as {
-    default: React.ComponentType
-    article: Article
-  }
+// async function importArticle(
+//   articleFilename: string,
+// ): Promise<ArticleWithSlug> {
+//   let { article } = (await import(`../app/articles/${articleFilename}`)) as {
+//     default: React.ComponentType
+//     article: Article
+//   }
 
-  return {
-    slug: articleFilename.replace(/(\/page)?\.mdx$/, ''),
-    ...article,
-  }
-}
+//   return {
+//     slug: articleFilename.replace(/(\/page)?\.mdx$/, ''),
+//     ...article,
+//   }
+// }
 
-export async function getAllArticles() {
-  let articleFilenames = await glob('*/page.mdx', {
-    cwd: './src/app/articles',
-  })
+// export async function getAllArticles() {
+//   let articleFilenames = await glob('*/page.mdx', {
+//     cwd: './src/app/articles',
+//   })
 
-  let articles = await Promise.all(articleFilenames.map(importArticle))
+//   let articles = await Promise.all(articleFilenames.map(importArticle))
 
-  return articles.sort((a, z) => +new Date(z.date) - +new Date(a.date))
-}
+//   return articles.sort((a, z) => +new Date(z.date) - +new Date(a.date))
+// }
