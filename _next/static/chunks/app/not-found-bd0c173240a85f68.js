c7426e6ab6b3b16c1ca427e4fdb15d5233785aa4
Polawat Huang <polawat_h@seasiacenter.com>
2024-03-26 15:23:43 +0700

    Updates

---
+++ _next/static/chunks/app/not-found-bd0c173240a85f68.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,701,453],{5215:function(n,e,u){Promise.resolve().then(u.t.bind(u,9254,23))}},function(n){n.O(0,[254,146,672,744],function(){return n(n.s=5215)}),_N_E=n.O()}]);
