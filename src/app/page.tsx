c358adf1073bff368880e6af10deb6aacbcac2d3
Polawat Huang <polawathuang@gmail.com>
2024-06-03 15:55:00 +0700

    Update content on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -136,10 +136,10 @@ function Newsletter() {
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
         <MailIcon className="h-6 w-6 flex-none" />
-        <span className="ml-3">Stay up to date</span>
+        <span className="ml-3">Send email to me</span>
       </h2>
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
-        Get notified when I publish something new, and unsubscribe at any time.
+        Feel free to contact me if you need further information.
       </p>
       <div className="mt-6 flex">
         <input
@@ -149,8 +149,19 @@ function Newsletter() {
           required
           className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
         />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
+      </div>
+      <div className="mt-4 flex">
+        <input
+          type="text"
+          placeholder="Some messages ..."
+          aria-label="Message"
+          required
+          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10"
+        />
+      </div>
+      <div className="mt-6 flex">
+        <Button type="submit" className="flex-none w-full">
+          Send
         </Button>
       </div>
     </form>
